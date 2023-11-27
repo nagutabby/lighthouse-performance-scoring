13e26f2d4469d13ead81ade753386619f7443722
nagutabby <62084485+nagutabby@users.noreply.github.com>
2023-11-27 23:10:03 +0900

    LighthouseのデータをCSVファイルに出力する

--- main.ts
+++ main.ts
@@ -3,19 +3,43 @@ import lighthouse, { Flags } from "lighthouse"
 import * as chromeLauncher from "chrome-launcher"
 
 const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
-const options: Flags = { logLevel: "info", output: "json", onlyCategories: ["performance"], port: chrome.port };
+
+const options: Flags = { logLevel: "info", output: "csv", port: chrome.port, onlyCategories: ["performance"] };
+const outputFile = "lighthouse.csv";
+
 const result = await lighthouse("https://example.com", options);
-const outputFile = "lighthouse_report.json"
+const lhr = result?.lhr;
+const output_array_title = ["requestedUrl", "category", "audit", "score", "displayValue", "description"];
+let output_arrays: string[][] = []
+
+const escape = value => `"${value.replace(/"/g, '""')}"`;
+const rowFormatter = row => row.map(value => {
+  if (value === null) return 'null';
+  return value.toString();
+}).map(escape);
+
+for (const category of Object.values(lhr!.categories)) {
+  for (const auditRef of category.auditRefs) {
+    const audit = lhr?.audits[auditRef.id];
+    if (!audit) continue;
+    output_arrays.push(rowFormatter([
+      lhr.requestedUrl,
+      category.id,
+      auditRef.id,
+      audit.score,
+      audit.displayValue || '',
+      audit.description,
+    ]));
+  }
+}
 
 if (!fs.existsSync(outputFile)) {
-  fs.writeFileSync(outputFile, "[]");
+  fs.writeFileSync(outputFile, output_array_title.join(",") + "\n")
 }
 
-const file = fs.readFileSync(outputFile);
-let reports: JSON[] = JSON.parse(file.toString());
-if (result !== undefined) {
-  reports.push(JSON.parse(result.report as string));
-  fs.writeFileSync(outputFile, JSON.stringify(reports, null, 4));
-}
+output_arrays.forEach((output_array) => {
+  fs.appendFileSync(outputFile, output_array.join(",") + "\n");
+})
+
 
 await chrome.kill();
